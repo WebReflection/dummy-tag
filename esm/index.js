24a73452aaa3de139f90847938c2eee061cecccc
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-07-04 00:01:56 +0200

    enough

--- esm/index.js
+++
@@ -1,6 +0,0 @@
-function dummyTag(t) {
-  for (var s = t[0], i = 1, l = arguments.length; i < l; i++)
-    s += arguments[i] + t[i];
-  return s;
-}
-export default dummyTag;
--- min.js
+++
@@ -1,1 +0,0 @@
-function dummyTag(t){for(var s=t[0],i=1,l=arguments.length;i<l;i++)s+=arguments[i]+t[i];return s}
